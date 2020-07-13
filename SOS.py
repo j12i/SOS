3d52a3886abed1555abd4ddad2b99a8e40a0f1ac
j12i <j12i@users.noreply.github.com>
2020-07-13 11:59:04 +0200

    Initial commit.

---
+++ SOS.py
@@ -0,0 +1,78 @@
+#!/usr/bin/python3
+from sys import argv
+from os import getcwd
+from sys import exit
+from subprocess import run
+from time import time
+from os.path import join,exists
+
+if len(argv)<2:
+    print('''Usage: SOS <soup name> <since> <working directory>
+  At least give the soup name as argument.
+  If you want to give wd but not since, enter 0.''')
+    exit(2^5)
+    
+soup = argv[1]
+page = argv[2] if len(argv)>2 and argv[2]!= '0' else None
+wd = argv[3] if len(argv)>3 else getcwd()
+
+wgetCommand = ['wget', '-t0', '-w0.5', '-e', 'robots=off', '-a', soup + '_log', '-nv', '--no-adjust-extension', '-x', '--save-cookies', 'cookies', '--keep-session-cookies', '--retry-on-http-error=503,500']
+haveCookies = exists(join(wd, 'cookies'))
+if haveCookies: wgetCommand.extend(['--load-cookies', 'cookies'])
+    
+downloadDir = join(wd, soup + '.soup.io')
+curLenOfNo = 9
+
+def downloadPage():
+    global haveCookies
+    starttime = time()
+    thisWgetCommand = wgetCommand.copy()
+    if page:
+        thisWgetCommand += ['https://' + soup + '.soup.io/since/' + page]
+        try:
+            run(thisWgetCommand, cwd = wd, check = True)
+        except:
+            print(' '.join(thisWgetCommand) + ' failed.')
+            exit(2)
+        if not haveCookies and exists(join(wd, 'cookies')):
+            wgetCommand.extend(['--load-cookies', 'cookies'])
+            haveCookies = True
+    else:
+        if not haveCookies:
+            firstWgetCommand = wgetCommand.copy()
+            firstWgetCommand.append('https://' + soup + '.soup.io/')
+            run(firstWgetCommand, cwd = wd)
+            wgetCommand.extend(['--load-cookies', 'cookies'])
+            haveCookies = True
+            thisWgetCommand = wgetCommand.copy()
+        thisWgetCommand += ['-H', '--domains', soup + '.soup.io,www.soup.io,asset.soup.io,static.soup.io', '-p', 'https://' + soup + '.soup.io/']
+        run(thisWgetCommand, cwd = wd)
+    print('got ' + (page if page != None else 'start') + ' in ' + str(round(time() - starttime)) + ' s')
+
+def extractNextPage():
+    global curLenOfNo, page
+    try:
+        pageFileName = '/since/' + page if page != None else '/index.html'
+        pageFile = open(downloadDir + pageFileName)
+    except IOError:
+        print('pageFile not found: ' + downloadDir + pageFileName)
+        exit(1)
+    with pageFile:
+        nextPage = None
+        for line in pageFile:
+            if line.startswith("      SOUP.Endless.next_url = '/since/"):
+                nextPage = line[38:38+curLenOfNo]
+                while nextPage[curLenOfNo-1] not in ['0','1','2','3','4','5','6','7','8','9']:
+                    curLenOfNo -= 1
+                nextPage = nextPage[:curLenOfNo]
+                break
+        if nextPage != None:
+            page = nextPage
+        else:
+            print('Last download corrupt or end of soup reached.\n\t' + page)
+            exit(4)
+
+
+while True:
+    downloadPage()
+    extractNextPage()
---
+++ readme.md
@@ -0,0 +1,11 @@
+This is my take on backing up your soup.io history. Just the HTML files from last to first, only on the first page the assets are saved to have a copy of the CSS styling your page. It doesn't take care of saving the pictures and videos in the posts after the first page. Essentially it is just a wrapper around wget, which does the heavy lifting like retrying, cookies etc.
+
+Prerequisites: Python 3 and wget.
+
+Usage: SOS <soup name> <since> <working directory>
+  At least give the soup name as argument.
+  If you want to give wd but not since, enter 0.
+
+Everything except the wget log and the cookie file will be saved in a <your>.soup.io directory inside the working directory.
+
+To save a private soup, start the script once, stop it, and copy the session cookie values for www.soup.io and <your>.soup.io as well as the soup_user_id cookie into the 'cookie' file in the working directory.
